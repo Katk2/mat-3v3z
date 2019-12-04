09c59948ae85e6b0da6001860dae465ce3aa3690
Tobias <tobias.j.hoerning@gmail.com>
2019-12-04 09:50:28 +0100

    Vi har lavet programmet KAn beregne kobinatorik, permutationer og kombinationer

--- Matematik/IKombinatorik.cs
+++ Matematik/IKombinatorik.cs
@@ -6,9 +6,29 @@ namespace Matematik
 {
     interface IKombinatorik
     {
-        public ulong BeregnFakultet(int n);
-        public ulong BeregnAntalPermutationer(int n, int r);
-        public ulong BeregnAntalKombinationer(int n, int r);
+        public ulong BeregnFakultet(ulong n, ulong Result = 1)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else if (n == 1)
+            {
+                return Result;
+            }
+            else
+            {
+                return BeregnFakultet(n-1, Result * n);
+            }
+        }
 
+        public ulong BeregnAntalPermutationer(ulong n, ulong r) //Check if r>n
+        {
+            return BeregnFakultet(n) / BeregnFakultet(n - r);
+        }
+        public ulong BeregnAntalKombinationer(ulong n, ulong r) //Check if r>n
+        {
+            return BeregnFakultet(n) / (BeregnFakultet(r) * BeregnFakultet(n - r));
+        }
     }
-}
+}   
